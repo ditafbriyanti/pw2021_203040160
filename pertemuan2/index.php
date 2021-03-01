c4da63e56842591fe48a1eea0e6ee955ab5a8a3f
Dita Febriyanti <ditafbriyanti@gmail.com>
2021-03-01 23:43:33 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,61 @@
+<?php
+/* 
+Dita Febriyanti
+203040160
+https://github.com/ditafbriyanti/pw2021_203040160
+Pertemuan 2 - Februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print 
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// Tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Dita Febriyanti";
+// echo "Halo, nama saya $nama";
+
+// Operator
+// Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// Penggabung string / concatenation / concat
+// .
+// $nama_depan = "Dita";
+// $nama_belakang = "Febriyanti";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Dita";
+// $nama .= " ";
+// $nama .= "Febriyanti";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
