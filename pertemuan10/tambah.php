0214b27f8e7f3bc82a608914270d9093a49f08a1
Dita Febriyanti <ditafbriyanti@gmail.com>
2021-05-06 20:40:40 +0700

    menyelesaikan video pertemuan 10

--- pertemuan10/tambah.php
+++ pertemuan10/tambah.php
@@ -29,6 +29,12 @@ if (isset($_POST['tambah'])) {
 <!DOCTYPE html>
 <html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tambah Data Mahasiswa</title>
+</head>
+
 <body>
 
   <h3>Form Tambah Data Mahasiswa</h3>
